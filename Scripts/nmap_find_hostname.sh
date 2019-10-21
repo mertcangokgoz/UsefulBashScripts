4d0b55fa6c5e6878f6d5d5c5b3177c2acbada4ad
siw3kosky <siw3kosky@gmail.com>
2019-10-21 18:37:44 +0200

    Create nmap_find_hostname.sh

---
+++ Scripts/nmap_find_hostname.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#### Script finds UP hosts and its HOSTNAME
+#### Usage:
+#### sh nmap_find_hostname.sh IP/MASK
+#### eg.: sh nmap_find_hostname.sh 192.168.0.0/16
+
+nmap -sn $1 -oG - | awk '$4=="Status:" && $5=="Up" {print $2, $3}'
